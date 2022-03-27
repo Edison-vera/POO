9f0ef4dd17b002174668803aecf424f5f310dbbd
Edison-vera <edisonefvc15@gmail.com>
2022-03-27 18:04:44 -0500

    Autoload de Classes

--- 08.php
+++ 08.php
@@ -1,4 +1,20 @@
 <?php include 'includes/header.php';
 
 
+//Incluir las otras clases 
+
+// require "clases/clientes.php";
+// require "clases/detalles.php";
+
+function mi_autoload($clase){
+    require __DIR__ . "/clases/" . $clase . ".php";
+}
+
+spl_autoload_register("mi_autoload");
+
+$detalles = new Detalles();
+echo"<br>";
+$clientes = new Clientes ();
+
+
 include 'includes/footer.php';
