18bf2d3192bad76cdafc215654fc4b8ce4c20ae6
Edison-vera <edisonefvc15@gmail.com>
2022-03-27 18:19:52 -0500

    Namespaces

--- 08.php
+++ 08.php
@@ -3,18 +3,32 @@
 
 //Incluir las otras clases 
 
-// require "clases/clientes.php";
-// require "clases/detalles.php";
+//  require "clases/clientes.php";
+//  require "clases/detalles.php";
 
-function mi_autoload($clase){
-    require __DIR__ . "/clases/" . $clase . ".php";
-}
+use App\Clientes;
+use App\Detalles;
 
-spl_autoload_register("mi_autoload");
+
+//Incluir las otras clases 
+ function mi_autoload($clase){
+    
+     $partes = explode("\\", $clase);
+     
+     require __DIR__ . "/clases/" . $partes[1] . ".php";
+ }
+
+ spl_autoload_register("mi_autoload");
+
+
+
+
 
 $detalles = new Detalles();
 echo"<br>";
 $clientes = new Clientes ();
+echo"<br>";
+
 
 
 include 'includes/footer.php';
