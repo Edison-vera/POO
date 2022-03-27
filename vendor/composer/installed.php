c90e0fa8a9bbe5a3e92ef00b9013a9104588a048
Edison-vera <edisonefvc15@gmail.com>
2022-03-27 18:48:08 -0500

    Autoload con Composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '18bf2d3192bad76cdafc215654fc4b8ce4c20ae6',
+        'name' => 'ediso/introduccion-phpoop_inicio',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ediso/introduccion-phpoop_inicio' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '18bf2d3192bad76cdafc215654fc4b8ce4c20ae6',
+            'dev_requirement' => false,
+        ),
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v6.1.0',
+            'version' => '6.1.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'reference' => 'fbb2967a3a68b07e37678c00c0cf51165051495f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
