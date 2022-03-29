2755693b22423161fee9de8ae8dd80515f5a47e5
Edison-vera <edisonefvc15@gmail.com>
2022-03-28 20:53:48 -0500

    Consultar la Base de datos con PDO

--- 10.php
+++ 10.php
@@ -1,4 +1,31 @@
 <?php include 'includes/header.php';
 
 
+//Conectar a la BD con PDO
+
+$db = new PDO("mysql:hots=localhost; dbname=bienes_raices", "root", "root", );
+
+//Creamos el query
+$query ="SELECT titulo, imagen FROM propiedades";
+
+//Lo preparamos
+$stmt = $db->prepare($query);
+
+//Lo ejecutamos
+$stmt->execute();
+
+//Obtenemos los resultados
+$resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+//Iteramos
+foreach($resultado as $propiedad):
+
+    echo $propiedad["titulo"];
+    echo"<br>";
+    echo $propiedad["imagen"];
+    echo"<br>";
+  
+endforeach;
+
+
 include 'includes/footer.php';
