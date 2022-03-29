013201c4b9dd2be4b7ff924cfd4181cb47ff7f6d
Edison-vera <edisonefvc15@gmail.com>
2022-03-28 20:34:06 -0500

    Consultar la Base de datos con POO y MySQLi

--- 09.php
+++ 09.php
@@ -1,4 +1,34 @@
 <?php include 'includes/header.php';
 
+//Conectar con la base de datos 
+
+$db = new mysqli("localhost", "root", "root", "bienes_raices");
+
+//Creamos el query
+
+$query ="SELECT titulo, imagen FROM propiedades";
+
+//Lo preparamos
+
+$stmt = $db->prepare($query);
+
+//Lo ejecutamos
+
+$stmt->execute();
+
+//Creamos la variable
+
+$stmt->bind_result($titulo, $imagen);
+
+//Asignamos el resultado
+
+$stmt->fetch();
+
+//Imprimimos el resultado 
+
+while($stmt->fetch()):
+var_dump($titulo);
+var_dump($imagen);
+endwhile;
 
 include 'includes/footer.php';
